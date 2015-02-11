da3ebb6f177af48b14471a92ac6d6c7aabc6a44c
Julia Knight <J@wr-130-64-92-116.medford.tufts.edu>
2015-02-11 16:06:04 -0500

    working version of tags - no tag view yet

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^new_post$', views.new_post, name='new_post'),
     url(r'^create_post$', views.create_post, name='create_post'),
     url(r'^(?P<post_id>\d+)/$', views.single_post, name='single_post'),
+    url(r'^(?P<tag_id>\d+)/$', views.tag, name='tag'),
 )
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -3,21 +3,42 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 
-from newsfeed.models import Post, User
+from newsfeed.models import Post, User, Tag
 
 def index(request):
     latest_post_list = Post.objects.order_by('-timestamp')
-    context = {'latest_post_list': latest_post_list}
+    tag_list = []
+    for post in latest_post_list:
+        tag_group = Tag.objects.filter(post_id=post)
+        tag_list.append(tag_group)
+    zipped_lists = zip(latest_post_list, tag_list)
+    context = {'zipped_lists': zipped_lists, 'latest_post_list': latest_post_list}
     return render(request, 'newsfeed/index.html', context)
 
 def new_post(request):
     return render(request, 'newsfeed/new_post.html')
 
 def create_post(request):
-    new_post = Post(post_text=request.POST['post_text'], post_title=request.POST['post_title'], timestamp=timezone.now(), user_id=User.objects.get(pk=1))
-    new_post.save()
-    return HttpResponseRedirect('/newsfeed')
+    if not request.POST['post_title']:
+        return HttpResponseRedirect('new_post')
+    elif not request.POST['post_text']:
+        return HttpResponseRedirect('new_post')
+    else:
+        new_post = Post(post_text=request.POST['post_text'], post_title=request.POST['post_title'], timestamp=timezone.now(), user_id=User.objects.get(pk=1))
+        new_post.save()
+        tag_string = request.POST['tags']
+        tag_list = tag_string.split(',')
+        for x in tag_list:
+            new_tag = Tag(tag=x, post_id=new_post)
+            new_tag.save()
+        return HttpResponseRedirect('/newsfeed')
 
 def single_post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'newsfeed/single_post.html', {'post': post})
+    tags = Tag.objects.filter(post_id=post)
+    return render(request, 'newsfeed/single_post.html', {'post': post, 'tags': tags})
+
+def tag(request, tag_id):
+    tagged_post_list = Post.objects.order_by('-timestamp')
+    context = {'tagged_post_list': tagged_post_list}
+    return render(request, 'newsfeed/tag.html', context)
