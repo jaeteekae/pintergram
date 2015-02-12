7342d76b8f30b7d6206aa5c5ccc56a4036edff25
James McCants <James.McCants@tufts.edu>
2015-02-11 23:07:23 -0500

    adds MEDIA_ROOT

--- pintergram/settings.py
+++ pintergram/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+#http://www.kelvinwong.ca/tag/media_root/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
