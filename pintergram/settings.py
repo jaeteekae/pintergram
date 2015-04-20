eb85af6dd3fc45f83b18f813dfe988e977154d34
James McCants <James.McCants@tufts.edu>
2015-04-20 13:05:21 -0400

    adds in buckett info for s3 and helps with static content

--- pintergram/settings.py
+++ pintergram/settings.py
@@ -114,6 +114,24 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
         'Cache-Control': 'max-age=94608000',
     }
 
+AWS_STORAGE_BUCKET_NAME = 'pintergram-static-media-content'
+AWS_ACCESS_KEY_ID = 'AKIAJ2CTDT5L6GWWLPZA'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 # When using unix domain sockets
 # Note: ``LOCATION`` needs to be the same as the ``unixsocket`` setting
 # in your redis.conf
